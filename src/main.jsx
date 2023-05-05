f0471c4913d4a076e9bc5fcc266fe481edb533a8
shafin bisswas <mdnazmussakibshafin@gmail.com>
2023-05-05 11:51:06 +0600

    create all header component and set up it as well

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Shop from './components/Shop/Shop.jsx';
+import Orders from './components/Orders/Orders.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "shop",
+        element: <Shop></Shop>
+      },
+      {
+        path: "order",
+        element: <Orders></Orders>
+      },
+      {
+        path: "inventory",
+        element: <Inventory></Inventory>
+      },
+      {
+        path: "login",
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
